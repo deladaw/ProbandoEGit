cb2b7dccabbf9ea299f75d81e181b6a4811384fc
Daniela Bru Soriano <danielabrudaw@gmail.com>
2021-01-06 19:35:49 +0100

    Pokemon Charmander

--- edeEgit/src/edeEgit/ProbandoEgit.java
+++ edeEgit/src/edeEgit/ProbandoEgit.java
@@ -5,9 +5,12 @@ public class ProbandoEgit {
 	public static void main(String[] args) {
 		
 		String nombrePokemon = "Pikachu";
+		String nombrePokemon = "Charmander";
 		
 		int pociones = 20;
 		int pokeballs = 5;
+		
+		
 	}
 
 }
